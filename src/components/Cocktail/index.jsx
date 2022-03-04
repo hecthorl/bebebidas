ad83203e0c0c06758257174ebf91e3d06de07f42
hector <hlvm03@gmail.com>
2022-03-04 10:10:36 -0500

    lazy load images

--- src/components/Cocktail/index.jsx
+++ src/components/Cocktail/index.jsx
@@ -1,40 +1,59 @@
-import { useEffect, useRef } from 'react'
+// import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import Tilt from 'react-parallax-tilt'
+import useBoolean from '../../hooks/useBoolean'
 import './index.css'
 
-export default function Cocktail(props) {
-   const { image, name, id, category, glass, alcohol } = props
+export default function Cocktail({
+   image,
+   name,
+   id,
+   category,
+   glass,
+   alcohol
+}) {
+   // const cocktailRef = useRef(null)
+   const [value, setValue] = useBoolean(true)
 
-   const cocktailRef = useRef(null)
-
-   useEffect(() => {
-      const observer = new IntersectionObserver((e) => {
-         e[0].target.style.display = 'none'
-         if (e[0].isIntersecting) {
-            e[0].target.style.display = 'block'
-            // console.log(e[0])
-         }
-      })
-      if (cocktailRef.current) {
-         observer.observe(cocktailRef.current)
-      }
-      return () => {
-         observer.unobserve(cocktailRef.current)
-      }
-   }, [])
+   // useEffect(() => {
+   //    const observer = new IntersectionObserver((e) => {
+   //       if (e[0].isIntersecting) {
+   //          console.log(e[0])
+   //       }
+   //    })
+   //    if (cocktailRef.current !== null) {
+   //       observer.observe(cocktailRef.current)
+   //    }
+   //    return () => {
+   //       if (cocktailRef.current !== null) {
+   //          observer.unobserve(cocktailRef.current)
+   //       }
+   //    }
+   // }, [])
 
    return (
       <div className="art-container">
          <article>
-            <Tilt scale={1} transitionSpeed={200} className="img-container">
+            <Tilt
+               scale={1}
+               transitionSpeed={200}
+               className="img-container"
+               tiltEnable={!value}
+            >
                <img
-                  ref={cocktailRef}
+                  // ref={cocktailRef}
                   alt={`Drink ${name}`}
                   loading="lazy"
                   src={image}
+                  onLoad={setValue.off}
                />
+               {value && (
+                  <div id="ab">
+                     <div />
+                  </div>
+               )}
             </Tilt>
+
             <div className="cocktail-body">
                <h3>{name}</h3>
                <div>
---
+++ src/hooks/useBoolean.js
@@ -0,0 +1,22 @@
+import { useRef, useState } from 'react'
+
+// eslint-disable-next-line no-self-compare
+const init = {} === {}
+
+/**
+ * Alternativa al useBoolean de chakra-UI
+ * @param {boolean} initVal Por defecto el valor siempre debe ser false
+ * @returns {Array<boolean, object>}
+ */
+const useBoolean = (initVal = init) => {
+   const [value, setValue] = useState(initVal)
+
+   const updateValue = useRef({
+      toggle: () => setValue((oldValue) => !oldValue),
+      on: () => setValue(true),
+      off: () => setValue(false)
+   })
+   return [value, updateValue.current]
+}
+
+export default useBoolean
